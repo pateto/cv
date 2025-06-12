cbdf5c8d1c6e27d286933667f70fa73e040bd853
pateto <alvaroecaceresl@gmail.com>
2025-06-12 11:34:05 -0500

    download pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'cv';
+
+  downloadPDF() {
+    if (typeof window === 'undefined') return; // Avoid SSR crash
+
+    import('html2pdf.js').then(html2pdf => {
+      const element = document.getElementById('pdf-content');
+      const opt = {
+        margin: 0.5,
+        filename: 'my-file.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+      };
+
+      html2pdf.default().set(opt).from(element).save();
+    });
+  }
+
 }
